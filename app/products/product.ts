200af1a844d03dc7786eafe8120863a73e3c183b
Sujit Singh <sujit.extentia@gmail.com>
2016-12-20 11:56:15 +0530

    adding APM code base

---
+++ app/products/product.ts
@@ -0,0 +1,10 @@
+export interface IProduct {
+    productId: number,
+    productName: String,
+    productCode: String,
+    releaseDate: string,
+    description: String,
+    price: number,
+    starRating: number,
+    imageUrl: String
+}
---
+++ app/products/propduct-filter.pipe.js
@@ -0,0 +1,1 @@
+//# sourceMappingURL=propduct-filter.pipe.js.map
